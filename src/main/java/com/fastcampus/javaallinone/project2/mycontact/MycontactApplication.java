7eb8f735aa5a1623b752006ed94e273603a7e6bb
yellowsummer <cshee9812@gmail.com>
2021-02-05 14:30:48 +0900

    first commit

---
+++ src/main/java/com/fastcampus/javaallinone/project2/mycontact/MycontactApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.javaallinone.project2.mycontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycontactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MycontactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastcampus/javaallinone/project2/mycontact/MycontactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.javaallinone.project2.mycontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycontactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
