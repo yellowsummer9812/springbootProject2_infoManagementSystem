4a9a8e9698a860c759d2c3503df3317af4b8b510
yellowsummer <cshee9812@gmail.com>
2021-02-06 11:13:21 +0900

    Lombok

--- src/main/java/com/fastcampus/javaallinone/project2/mycontact/domain/Person.java
+++ src/main/java/com/fastcampus/javaallinone/project2/mycontact/domain/Person.java
@@ -1,14 +1,12 @@
 package com.fastcampus.javaallinone.project2.mycontact.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.Accessors;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
 @Entity
 @Data
@@ -21,7 +19,22 @@ public class Person {
     @GeneratedValue // 자동적으로 생성
     private Long id;
 
+    @NonNull
     private String name;
 
+    @NonNull
     private int age;
+
+    private String hobby;
+
+    @NonNull
+    private String bloodType;
+
+    private String address;
+
+    private LocalDate birthday;
+
+    private String job;
+
+    private String phoneNumber;
 }
