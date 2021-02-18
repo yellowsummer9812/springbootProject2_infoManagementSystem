26087edf9c07644219ff447c2962d4f756152cc6
yellowsummer <cshee9812@gmail.com>
2021-02-19 00:58:41 +0900

    Parameter Validator

--- src/main/java/com/fastcampus/javaallinone/project2/mycontact/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/fastcampus/javaallinone/project2/mycontact/exception/handler/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import com.fastcampus.javaallinone.project2.mycontact.exception.dto.ErrorRespons
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -23,6 +24,12 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handlePersonNotFountException(PersonNotFoundException ex){
         return ErrorResponse.of(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        return ErrorResponse.of(HttpStatus.BAD_REQUEST, ex.getBindingResult().getFieldError().getDefaultMessage());
     }
 
     // 최상위의 exception
--- src/test/java/com/fastcampus/javaallinone/project2/mycontact/controller/PersonControllerTest.java
+++ src/test/java/com/fastcampus/javaallinone/project2/mycontact/controller/PersonControllerTest.java
@@ -100,8 +100,38 @@ class PersonControllerTest {
                 MockMvcRequestBuilders.post("/api/person")
                     .contentType(MediaType.APPLICATION_JSON_UTF8)
                     .content(toJsonString(dto)))
-                .andExpect(jsonPath("$.code").value(500))
-                .andExpect(jsonPath("$.message").value("알 수 없는 서버 오류가 발생했습니다."));
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("이름은 필수값입니다."));
+    }
+
+    @Test
+    void postPersonIfNameIsEmptyString() throws Exception{
+        PersonDto dto = new PersonDto();
+        dto.setName("");
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/person")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .content(toJsonString(dto)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("이름은 필수값입니다."));
+
+    }
+
+    @Test
+    void postPersonIfNameIsBlankString() throws Exception{
+        PersonDto dto = new PersonDto();
+        dto.setName(" ");
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/person")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .content(toJsonString(dto)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("이름은 필수값입니다."));
     }
 
     @Test
